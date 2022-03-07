0815a985f9af9af8956b877881b44938166c3ae5
unknown <davca341@student.liu.se>
2022-03-07 11:11:54 +0100

    user methods

--- server/main.py
+++ server/main.py
@@ -30,16 +30,19 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   email = db.Column(db.String, nullable = False)
   name = db.Column (db.String, nullable = False)
-  isAdmin =db.Column(db.Boolean, nullable = False)
+  is_admin =db.Column(db.Boolean, default = False, nullable = False)
 
+  def __repr__(self):
+    return '<User {}: {} {}>'.format(self.id, self.email, self.name)
+
+  def serialize(self):
+    return dict(id=self.id, email=self.email, name=self.name, is_admin=self.is_admin)
 
 
 @app.route('/product/<int:id>', methods = ['GET', 'DELETE', 'PUT'] )
 def product(product_id):
     if request.method == 'GET':
       print("hello")
-
-
 
 
 @app.route('/product', methods = ['GET', 'POST'] )
@@ -62,6 +65,43 @@ def products():
     return i
 
   return "401"
+
+@app.route('/user/<int:user_id>', methods = ['GET', 'PUT', 'DELETE'])
+def users(user_id):
+  if request.method == 'GET':
+    temp = User.query.filter_by(id = user_id).first_or_404()
+    return jsonify(temp.serialize())
+  elif request.method == 'PUT':
+    user = request.get_json()
+    user["id"] = user_id
+    User.query.filter_by(id = user_id).update(user)
+    temp = User.query.filter_by(id = user_id).first_or_404()
+    db.session.commit()
+    return jsonify(temp.serialize())
+  elif request.method == 'DELETE':
+    temp_user = User.query.get_or_404(user_id)
+    db.session.delete(temp_user)
+    db.session.commit()
+    return "200 OK"
+
+@app.route('/user', methods = ['GET', 'POST'])
+def user():
+  if request.method == 'GET':
+    user = User.query.all()
+    user_list = []
+
+    for x in user:
+      user_list.append(x.serialize())
+    return jsonify(user_list)
+  
+  elif request.method == 'POST':
+    new_user = request.get_json()
+    x = User(email = new_user["email"], name = new_user["name"])
+    db.session.add(x)
+    db.session.commit()
+    user_id = x.id
+    i = User.serialize(User.query.get_or_404(user_id))
+    return i
 
 if __name__ == "__main__":
   app.run(debug=True)
