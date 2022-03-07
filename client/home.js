9b73621b105f72a77f58932dcc481e2ad6ab31a1
Unn Zachrison <unnza234@student.liu.se>
2022-03-07 14:41:01 +0100

    sidebar

--- client/home.js
+++ client/home.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-=======
-
->>>>>>> 580b02d52d493e9612de14892601f5ef5319801a
     // Kod i detta block körs när dokumentet laddats klart.    
     $(".container").html($("#view-home").html())  
  })
@@ -17,12 +13,9 @@ $('#basketButton').click(function (e) {
   $("#basketModal").modal('toggle');
   e.preventDefault();
 });
-<<<<<<< HEAD
-=======
 
 $('#homeButton').click(function (e) {
   $(".container").html($("#view-home").html())  
     e.preventDefault();
 });
 
->>>>>>> 580b02d52d493e9612de14892601f5ef5319801a
