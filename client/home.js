ee7ff620d589832767bd48700308e29e8ebd1763
Oscar Erik Josef Söderlund <oscso651@student.liu.se>
2022-03-07 11:02:02 +0100

    basket is evolving

--- client/home.js
+++ client/home.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     // Kod i detta block körs när dokumentet laddats klart.    
-    alert("siddan laddades");
     $(".container").html($("#view-about").html())  
  })
 
 $('#aboutButton').click(function (e) {
-    alert("siddan kom hit");
     $(".container").html($("#view-about").html())  
     e.preventDefault();
   });
+
+$('#basketButton').click(function (e) {
+  $("#basketModal").modal('toggle');
+  e.preventDefault();
+});
