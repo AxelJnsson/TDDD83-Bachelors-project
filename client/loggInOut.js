07bdf0ab5b3eb47b0edb5e5834340d02d8bd18ba
Oscar Erik Josef Söderlund <oscso651@student.liu.se>
2022-03-08 18:26:29 +0100

    fixade login halvt samt knappen till logga ut

---
+++ client/loggInOut.js
@@ -0,0 +1,26 @@
+$('#loginButton').click(function (e) {
+    e.preventDefault();
+    $("#loginModal").modal('toggle');
+  });
+
+$('#closeLoginButton').click(function (e) {
+    e.preventDefault();
+    $("#loginModal").modal('hide');
+  });
+
+$('#xButtonLogin').click(function (e) {
+  e.preventDefault();
+  $("#loginModal").modal('hide');
+});
+
+$('#LoginButton').click(function (e) {
+    e.preventDefault();
+    var inputEmail = $("#inputEmailForm").val();
+    var inputPassword = $("#inputPasswordForm").val();
+    alert("Du försökte logga in med\n emailen: " + inputEmail+ "\noch lösenordet: "+inputPassword+"\n Men asså det går inte att logga in än.");
+    $("#loginModal").modal('hide');
+  });
+  
+$('#logoutButton').click(function (e) {
+  alert("vi får väll låtsas att du loggade ut nu")
+  });
