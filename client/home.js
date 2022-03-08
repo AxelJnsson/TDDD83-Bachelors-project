3aacc390d06b50bdcaf9ce57394ac007d71d5a69
Oscar Erik Josef Söderlund <oscso651@student.liu.se>
2022-03-08 10:17:43 +0100

    commit innan pull

--- client/home.js
+++ client/home.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-    // Kod i detta block körs när dokumentet laddats klart.  
-    alert("siddan laddades");
-    $(".container").html($("#view-home").html())  
- });
-
-$('#aboutButton').click(function (e) {
-    e.preventDefault();
-    alert("siddan kom hit");
-    $(".container").html($("#view-about").html())  
-  });
-=======
     // Kod i detta block körs när dokumentet laddats klart.    
     $(".container").html($("#view-about").html())  
  })
@@ -24,4 +12,3 @@ $('#basketButton').click(function (e) {
   $("#basketModal").modal('toggle');
   e.preventDefault();
 });
->>>>>>> c38a64ef3c7b7ce768761ab204d21eaacc57e33a
