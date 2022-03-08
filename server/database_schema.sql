6795949d0157bdbc8cae4f0f3e7cc9f16471dec3
unknown <davca341@student.liu.se>
2022-03-08 16:43:31 +0100

    created database_schema

---
+++ server/database_schema.sql
@@ -0,0 +1,50 @@
+
+
+DROP TABLE IF EXISTS product CASCADE;
+DROP TABLE IF EXISTS user CASCADE;
+DROP TABLE IF EXISTS used_product CASCADE;
+DROP TABLE IF EXISTS new_product CASCADE;
+
+SELECT 'Creating tables' AS 'Message';
+
+CREATE TABLE product (
+    product_id INT,
+    brand VARCHAR NOT NULL,
+    model VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    price INT NOT NULL,
+    color VARCHAR NOT NULL,
+    year INT NOT NULL,
+    type VARCHAR NOT NULL,
+    new_or_not BOOLEAN NOT NULL,
+    seller INT,
+    CONSTRAINT pk_product PRIMARY KEY (product_id)
+) 
+
+CREATE TABLE user (
+    user_id INT,
+    email VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    password_hash VARCHAR NOT NULL,
+    CONSTRAINT pk_user PRIMARY KEY (user_id)
+)
+
+CREATE TABLE used_product (
+    SELECT A.*, B.*
+    FROM product A, user B
+    WHERE B.user_id IN (SELECT seller
+                        FROM product
+                        WHERE new_or_not = '0';)
+    DROP COLUMN A.seller
+    DROP COLUMN A.new_or_not
+
+)
+
+CREATE TABLE new_product (
+    SELECT *
+    FROM product
+    WHERE new_or_not = '1';
+    DROP COLUMN new_or_not
+)
