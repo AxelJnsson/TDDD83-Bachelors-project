1b18aad2ca239f7676b4967819735282f41a0ead
Anna Petersson <annpe185@student.liu.se>
2022-03-11 09:53:50 +0100

    mer dynamisk display

--- client/productview.js
+++ client/productview.js
@@ -20,9 +20,11 @@ function createGuitar(){
     let testgitarr = new Product("1", "märke", "modell", "bra gitarr", "100 kr", "svart", "2000", "skitfulgitarr.png", "helt fantastisk");
     let testgitarr2 = new Product("2", "giiiiitarrrrrr", "m", "ännu bättre", "2000 rubel", "rosa", "2020", "gitarklassisk.jpg", "nyskick");
     let testgitarr3 = new Product("3", "bäst", "aaa", "carl bildt", "2000 rubel", "blå", "1948","carlbildt.png", "mycket bra skick" );
+    let testgitarr4 = new Product("4", "bäst", "aaa", "carl bildt", "2000 rubel", "blå", "1948","carlbildt.png", "mycket bra skick" );
+    let testgitarr5 = new Product("5", "märke", "modell", "bra gitarr", "100 kr", "svart", "2000", "skitfulgitarr.png", "helt fantastisk");
 
 
-    const gitarrer = [testgitarr, testgitarr2, testgitarr3];
+    const gitarrer = [testgitarr, testgitarr2, testgitarr3, testgitarr4, testgitarr5];
     return gitarrer;
 }
 
@@ -42,16 +44,23 @@ function createGuitar(){
     let guitars = createGuitar();
     //alert(guitars.length);
 
-    //alert("produktinfon skriver just nu ut flera olika beskrivningar pga test av loop")
+    let j = 0;
+    //alert(j);
+
     for (let i=0; i < guitars.length; i++) {
+      
+        if (i%3 == 0) {
+            j++;
+        }
 
-        //$(".product-modal-body").append("<b>Produktnamn:</b> " + guitars[i].name + "<br> <b>Märke:</b> " + guitars[i].brand + "<br> <b>Modell:</b> " + guitars[i].model + "<br> <b>Pris:</b> " + guitars[i].price + "<br><br>");
+       $("#testdiv").append("<div class='row' id='"+j+"'></div>");
+ 
+       $("#"+j).append("<div class='col-sm'><div class='card'><img class='card-img-top'  src='"+ guitars[i].image +"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>" + guitars[i].name + "</h5><p class='card-text'> <b>Märke:</b> " + guitars[i].brand + "<br> <b>Modell:</b> " + guitars[i].model + "<br> <b>Pris:</b> " + guitars[i].price + "</p></div>" + "<button class='btn btn-primary btnInfo' data-id='"+ i + "'>info</button></div></div>");
 
-        $("#testrow").append("<div class='col-sm'><div class='card'><img class='card-img-top'  src='"+ guitars[i].image +"' alt='Card image cap'><div class='card-body'><h5 class='card-title'>" + guitars[i].name + "</h5><p class='card-text'> <b>Märke:</b> " + guitars[i].brand + "<br> <b>Modell:</b> " + guitars[i].model + "<br> <b>Pris:</b> " + guitars[i].price + "</p></div>" + "<button class='btn btn-primary btnInfo' data-id='"+ i + "'>info</button></div></div>");
-
-       
+        
 
     }
+
     $('.btnInfo').on("click" ,function (e) {
         var prod_id = $(this).data('id');
         //alert(prod_id);
