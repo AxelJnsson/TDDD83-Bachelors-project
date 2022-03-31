9744b27ccae46b7e8d669b5f71fb604978154e26
Oscar Erik Josef Söderlund <oscso651@student.liu.se>
2022-03-31 15:22:37 +0200

    framsteg

--- client/basket.js
+++ client/basket.js
@@ -18,19 +18,17 @@ $('#shopFromBasketButton').click(function (e) {
   e.preventDefault();
   $("#basketModal").modal('hide');
   $("#mainViewContainer").html($("#view-cashregister").html());
-  var div = document.getElementById('totalsumLine');
-  div.innerHTML += "Total: " + "1023kr"
-
-
-
-
+  printBasketedProducts()
   });
 
-// function toCheckout(){
-//   $("#basketModal").modal('hide');
-//   $(".container").html($("#view-cashregister").html());
-// }
 
 function printBasketedProducts(){
+  var scrollableProductDiv = document.getElementById('scrollableItemsInBasket')
+  for (let i=0; i < 6; i++){
+    scrollableProductDiv.innerHTML += '<div id="productInScrollableAreaDiv'+i+'"> Produkt '+i+' <button id="removeProductFromRegister'+i+'"> <img src="soptunnapixil.png" width="25" height="30"> </button>  </div> <br>'
+  }
+    var div = document.getElementById('totalsumLine');
+  div.innerHTML += "Total: " + "1023kr"
    // alert("När vi har fixat lite varor så kommer dom att dyka upp i varukorgen. Men just nu är den permanent tom.");
 }
+
