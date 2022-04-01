5b3ef9dee03a092cb5ead84c58b72347c4711dc1
unknown <davca341@student.liu.se>
2022-04-01 13:00:13 +0200

    trying to add functionality for editing a user

--- client/accountPage.js
+++ client/accountPage.js
@@ -10,7 +10,12 @@ class User {
 }
 //let active_user = new User(3, "Fredrik.Lindberg@gmail.com", "Fredrik", "Lindberg", True);
 
-
+$('#editUserButton').click(function (e) {
+  alert("1");
+  e.preventDefault();
+  $("#editUserModal").modal('toggle');
+  alert("2");
+});
 
 function displayUser() {
    
@@ -23,20 +28,33 @@ function displayUser() {
         type: 'GET',
         success: function(u) {
             
-      var anv = u;
-      let active_user = new User("3", "Fredrik.Lindberg@gmail.com", "Fredrik", "Lindberg", "Åbylundsgatan", "True");
-    $("#profname").append("<h5 class='card-title'> "+anv.first_name + " " + anv.last_name +"</h5>");   
-    $("#profAdress").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+active_user.adress+">");
-    $("#profContact").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.email+">");
-    $("#profSek").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.is_admin+">");
- 
-    
-    }
-});
-
-function changeUser() {
-  
+          var anv = u;
+          let active_user = new User("3", "Fredrik.Lindberg@gmail.com", "Fredrik", "Lindberg", "Åbylundsgatan", "True");
+          $("#profname").append("<h5 class='card-title'> "+anv.first_name + " " + anv.last_name +"</h5>");   
+          $("#profAdress").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+active_user.adress+">");
+          $("#profContact").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.email+">");
+          $("#profSek").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.is_admin+">");
+        }
+    });
 }
 
-
+function editUser() {
+  
+  var x = JSON.parse(sessionStorage.getItem('auth')).user.user_id; 
+      
+    $.ajax({
+      headers: {
+        "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+      url:'/user/' + x,
+      type: 'GET',
+      success: function(u) {
+          
+        var anv = u;
+        let active_user = new User("3", "Fredrik.Lindberg@gmail.com", "Fredrik", "Lindberg", "Åbylundsgatan", "True");
+        $("#profname").append("<h5 class='card-title'> "+anv.first_name + " " + anv.last_name +"</h5>");   
+        $("#profAdress").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+active_user.adress+">");
+        $("#profContact").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.email+">");
+        $("#profSek").append("<input type='text' readonly class='form-control-plaintext' id='staticEmail' value="+anv.is_admin+">");
+      }
+    });
 }
