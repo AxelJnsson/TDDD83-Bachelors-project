61b92599708b6b57f73effc7430c7bd405c28df9
Oscar Erik Josef Söderlund <oscso651@student.liu.se>
2022-04-05 18:49:08 +0200

    klarfördagen

--- client/javascript/basket.js
+++ client/javascript/basket.js
@@ -1,3 +1,5 @@
+//BASKET
+
 $('#basketButton').click(function (e) {
     e.preventDefault();
     $("#basketModal").modal('toggle');
@@ -14,33 +16,9 @@ $('#xButtonBasket').click(function (e) {
   $("#basketModal").modal('hide');
   });
 
-$('#shopFromBasketButton').click(function (e) {
-  e.preventDefault();
-  $("#basketModal").modal('hide');
-  $("#mainViewContainer").html($("#view-cashregister").html());
-  printBasketedProducts()
-  });
-
-// $('#addProductToCart').click(function (e) {
-//   e.preventDefault();
-//   alert("hjej")
-//   });
 
 function addProductToCart(productToAdd){
   alert(productToAdd)
-  //var temUserID = JSON.parse(sessionStorage.getItem('auth')).user.user_id;
-  //   $.ajax({
-  //     headers: {
-  //       "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},    
-  //     url:'/user/'+temUserID,
-  //     type: 'GET',
-  //     success: function(u) { 
-  //       alert(u)         
-  //     },
-  //     error: function(){
-  //         alert("la inte till");
-  //     }    
-  // });
     $.ajax({    
       headers: {
         "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},    
@@ -53,57 +31,11 @@ function addProductToCart(productToAdd){
           alert("funkarej");
       }    
   });
-  //   $.ajax({        
-  //     headers: {
-  //       "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
-  //     url:'/product/'+productToAdd+'/unadding',
-  //     type: 'POST',
-  //     success: function(u) { 
-  //       alert("tog bort")       
-  //     },
-  //     error: function(){
-  //         alert("tog inte bort");
-  //     }    
-  // });
-}
-
-  
-function printBasketedProducts(){
-  var scrollableProductDiv = document.getElementById('scrollableItemsInBasket')
-  for (let i=0; i < 6; i++){
-    let id = "productInScrollableAreaDiv"+i;
-    scrollableProductDiv.innerHTML += '<div id="productInScrollableAreaDiv'+i+'"> Produkt '+i+' <button id="removeProductFromRegister'+i+'" value="'+id+'" onclick="removeFromRegister(this.value)"> <img src="/images/soptunnapixil.png" width="25" height="30"> </button> </div> <div id="breakBetweenProducts'+i+'"> <br> </div>'
-  }
-    var div = document.getElementById('totalsumLine');
-    div.innerHTML += "Total: " + "1023kr"
-   // alert("När vi har fixat lite varor så kommer dom att dyka upp i varukorgen. Men just nu är den permanent tom.");
-}
-
-function removeFromRegister(id){
-  var allabarn = document.getElementById(id)
-  allabarn.remove();
-  var breakBetween = document.getElementById("breakBetweenProducts"+id[id.length-1])
-  breakBetween.remove();
-}
-
-function stripeTestFunction(){
-  $.ajax ({
-    headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
-    url:'/create-checkout-session',
-    type: 'POST',
-    datatype: 'JSON',
-    contentType: "application/json",
-    data: JSON.stringify({
-      "price":123123}),
-    success: function(data) {    
-      return stripe.redirectToCheckout({sessionId: data.sessionId})
-    }
-  }); 
 }
 
 function getProductsToPrintInBasket(userID){
+  $('#bodyBasketModal').empty();
   $.ajax ({
-    // headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
     url:'/user/'+userID,
     type: 'GET',
     datatype: 'JSON',
@@ -111,8 +43,6 @@ function getProductsToPrintInBasket(userID){
     success: function(data) {
       arrayOfProducts = []
       data[2].forEach(element =>arrayOfProducts.push(element))
-
-      
       showInBasketModal(arrayOfProducts)
     }
   }); 
@@ -122,7 +52,6 @@ function showInBasketModal(products){
 
   for (let i = 0; i <products.length; i++)
     $.ajax ({
-      // headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
       url:'/product/'+products[i].product_id,
       type: 'GET',
       datatype: 'JSON',
@@ -134,9 +63,86 @@ function showInBasketModal(products){
 }
 
 function printProductInBasketModal(product){
+  $('#bodyBasketModal').append('<div id="productDivInBaskedModal">  <img src='+ product.image +' style="height: 150px; width: 150px;">  <div style=""> '+product.name+' <br> '+product.price+'kr </div> <button class="deleteProductFromCartButton" onClick="deleteProductFromCart(this.value)" value="'+product.product_id+'"> <img src="/images/soptunnapixil.png" width="25" height="30"> </button>  </div> <br>');
+}
+
+function deleteProductFromCart(productID){
+  $.ajax ({
+    headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+    url:'/product/'+productID+'/unadding',
+    type: 'POST',
+    datatype: 'JSON',
+    contentType: "application/json",
+
+    success: function(product) {
+      alert("tog bort")
+      getProductsToPrintInBasket(JSON.parse(sessionStorage.getItem('auth')).user.user_id);
+    },
+    error: function(u){
+      alert("tog inte bort fk u");
+    } 
+  });
+}
 
 
 
-  
 
+
+//KASSA
+
+
+
+$('#shopFromBasketButton').click(function (e) {
+  e.preventDefault();
+  $("#basketModal").modal('hide');
+  $("#mainViewContainer").html($("#view-cashregister").html());
+  printBasketedProducts(JSON.parse(sessionStorage.getItem('auth')).user.user_id)
+  });
+
+function printBasketedProducts(userID){
+  $.ajax ({
+    url:'/user/'+userID,
+    type: 'GET',
+    datatype: 'JSON',
+    contentType: "application/json",
+    success: function(data) {
+      arrayOfProducts = []
+      data[2].forEach(element =>arrayOfProducts.push(element))
+      showInRegister(arrayOfProducts)
+    }
+  }); 
+}
+
+function showInRegister(products){
+
+  for (let i = 0; i <products.length; i++)
+  $.ajax ({
+    url:'/product/'+products[i].product_id,
+    type: 'GET',
+    datatype: 'JSON',
+    contentType: "application/json",
+    success: function(product) {
+      printProductInBasketRegister(product);
+    }
+  });
+
+}
+
+function printProductInBasketRegister(product){
+  $('#scrollableItemsInBasket').append("asdasdasdas");
+}
+
+function stripeTestFunction(){
+  $.ajax ({
+      headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+      url:'/create-checkout-session',
+      type: 'POST',
+      datatype: 'JSON',
+      contentType: "application/json",
+      data: JSON.stringify({
+      "price":123123}),
+      success: function(data) {    
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+      }
+  }); 
 }
