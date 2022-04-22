4fc025d427f42f8ce7e856a90f96a54bed48f178
Joel Nilsson <joeni320@student.liu.se>
2022-04-22 11:54:13 +0200

    bugfix för fraktsedel

--- client/javascript/checkout.js
+++ client/javascript/checkout.js
@@ -81,7 +81,6 @@ var payWithCard = function(stripe, card, clientSecret) {
         // The payment succeeded!
         orderComplete(result.paymentIntent.id);
         //addOrdersAndItemsToHistory();
-        SucPay();
         createShipping();
       
         
--- client/javascript/home.js
+++ client/javascript/home.js
@@ -454,9 +454,6 @@ function regOrAnnons() {
 function SucPay(u) {
   
   $("#mainViewContainer").html($("#lyckad-betalning").html())
-  alert("din betalning gick igenom");
-  addOrdersAndItemsToHistory();
-  //Gör nåt som uppdaterar varukorgen och skapar en
 
   $("#lyckadBet").attr("href", u)
 
