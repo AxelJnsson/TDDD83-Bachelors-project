1aaa6f3819d093284febd7624e4c9eb1207a1dc8
unknown <davca341@student.liu.se>
2022-04-22 12:56:17 +0200

    Revert "refaktorering"
    This reverts commit 3c28b842235d266774d747ee786bad2607b03132.

---
+++ labfiles/client.js
@@ -0,0 +1,285 @@
+//asdfghjkl
+$(document).ready(function() {   
+
+    $(".container").html($("#view-home").html());
+    var signedIn;
+    if ((sessionStorage.getItem('auth') == null) || sessionStorage.getItem('auth').token <= 0) {
+      signedIn = true;
+    } else {
+      signedIn = false;
+    }
+
+
+    $('#registerButton').toggleClass('d-none', !signedIn);
+    $('#loginButton').toggleClass('d-none', !signedIn);
+    $('#logoutButton').toggleClass('d-none', signedIn);
+   
+});
+
+$('#contactButton').click(function (e) {
+   /* e.preventDefault(); */  
+    $(".container").html($("#view-contact").html())
+    $("#skickaknapp").append('<button type="button" class="btn btn-tonehub" id="sendButton" onclick="submitmsg()">Skicka</button>');
+});
+
+$('#homeButton').click(function (e) {
+  $(".container").html($("#view-home").html())
+ 
+
+ /* e.preventDefault(); */
+});
+
+$('#carsButton').click(function (e) {
+  $(".container").html($("#view-cars").html())
+  var signedIn;
+  if ((sessionStorage.getItem('auth') == null) || sessionStorage.getItem('auth').token <= 0) {
+    signedIn = true;
+  } else {
+    signedIn = false;
+  }
+    $('#add-car').toggleClass('d-none', signedIn);
+  showCars();
+ /* e.preventDefault(); */
+});
+
+$('#registerButton').click(function (e) {
+  $(".container").html($("#view-signup").html())
+  //alert("registrera");
+  $("#signupknapp").append(' <button type="button" class="btn btn-tonehub" id="signupButton" onclick="signUp()">Registrera</button>');
+ /* e.preventDefault(); */
+});
+
+$('#loginButton').click(function (e) {
+  $(".container").html($("#view-login").html())
+  
+ 
+  $("#loggainknapp").append('<button type="button" class="btn btn-tonehub" id="loginButton" onclick="logIn()">Logga in</button>');
+ /* e.preventDefault(); */
+});
+
+$('#logoutButton').click(function (e) {
+  $(".container").html($("#view-home").html())
+ sessionStorage.removeItem('auth');
+ alert("Du loggas ut!");
+ window.location.reload();
+
+ /* e.preventDefault(); */
+});
+
+
+
+
+
+function showCars(){  
+  $.ajax ({
+    headers: {
+      "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+      url: '/cars',
+      type: 'GET',
+
+      success: function(cars) {
+  
+    var list = cars;
+    
+
+  for (let i = 0; i < list.length; i++) {      
+      $(".card-container").append('<div class="card" style="width:400px"><div class="card-body"><h4 class="card-title"></h4><p class="card-text"></p></div></div>');                    
+
+      let cards = $('.card-text');
+      var admin =  JSON.parse(sessionStorage.getItem('auth')).user.isadmin;
+      var x = JSON.parse(sessionStorage.getItem('auth')).user.id;
+      
+        
+       if (list[i].owner != null){ 
+            if (list[i].owner.id == JSON.parse(sessionStorage.getItem('auth')).user.id) {        
+           cards.eq(i).html(("Id: " + list[i].id + "<br>"+ "Make: " + list[i].make + "<br>" + "Model: " + list[i].model + "<br>" + "Bokad av: " + list[i].owner.name + "<br>" + '<a class="btn btn-tonehub deletebutton" data-id = "'+list[i].id+'" href="#">Delete</a>' + '<a class="btnbtn-tonehub editbutton" data-id = "'+list[i].id+'" data-toggle="modal" data-target="#exampleModal" href="#">Edit</a>' + '<a class="btn btn-tonehub unbookbutton" data-id = "'+list[i].id+'" href="#">Unbook</a>'));
+            } else {
+              cards.eq(i).html(("Id: " + list[i].id + "<br>"+ "Make: " + list[i].make + "<br>" + "Model: " + list[i].model + "<br>" + "Bokad av: " + list[i].owner.name + "<br>" + '<a class="btn btn-tonehub deletebutton" data-id = "'+list[i].id+'" href="#">Delete</a>' + '<a class="btn btn-tonehub editbutton" data-id = "'+list[i].id+'" data-toggle="modal" data-target="#exampleModal" href="#">Edit</a>'));
+
+            }
+          } else {
+           cards.eq(i).html(("Id: " + list[i].id + "<br>"+ "Make: " + list[i].make + "<br>" + "Model: " + list[i].model + "<br>" + '<a class="btn btn-tonehub deletebutton" data-id = "'+list[i].id+'" href="#">Delete</a>' + '<a class="btn btn-tonehub editbutton" data-id = "'+list[i].id+'" data-toggle="modal" data-target="#exampleModal" href="#">Edit</a>' +'<a class="btn btn-tonehub bookbutton" data-id = "'+list[i].id+'" href="#">Book</a>'));           
+           
+          }         
+  }
+
+    $('.deletebutton').toggleClass('d-none', !admin);
+    $('.editbutton').toggleClass('d-none', !admin);
+    $('.bookbutton').toggleClass('d-none', admin);
+    $('.unbookbutton').toggleClass('d-none', admin);
+    $('#add-car').toggleClass('d-none', !admin);
+    
+
+  $('.deletebutton').click(function (e) {        
+    var car_id = $(this).data('id');
+    $.ajax ({
+      headers: {
+        "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+      url:'/cars/' + car_id,
+      type: 'DELETE',
+
+      success: function(cars) {    
+        alert("Bilen tas bort");
+        $(".container").html($("#view-cars").html())   
+        showCars();        
+      }
+    })
+  });  
+  
+
+  $('.editbutton').click(function (e) {   
+   var car_id = $(this).data('id');
+   $.ajax({
+    headers: {
+      "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+    url:'/cars/' + car_id,
+    type: 'GET',
+    success: function(cars) {
+      var car = cars;
+      $('#inputId').val(car.id);    
+      $('#inputMake').val(car.make);      
+      $('#inputModel').val(car.model);
+      $('#inputCustomer').val(car.owner.id);
+    }
+   })   
+   }); 
+   
+   $('.bookbutton').click(function (e) {        
+    var car_id = $(this).data('id');  
+    $.ajax({
+     headers: {
+        "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+     url:'/carsb/' + car_id+ '/booking',
+     type: 'POST',
+     datatype: 'JSON',
+    contentType: "application/json",
+    data: JSON.stringify({"id" : car_id}), 
+     success: function(info) {
+      $(".container").html($("#view-cars").html()) 
+        showCars();
+     }
+    })   
+    }); 
+
+    $('.unbookbutton').click(function (e) {        
+      var car_id = $(this).data('id');
+     
+      $.ajax({
+       headers: {
+          "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+       url:'/carsb/' + car_id+ '/unbook',
+       type: 'POST',
+       datatype: 'JSON',
+      contentType: "application/json",
+      data: JSON.stringify({"id" : car_id}), 
+       success: function(info) {
+          alert(info);
+          $(".container").html($("#view-cars").html()) 
+          showCars();
+       }
+      })   
+      }); 
+   
+}})
+}
+
+//add car
+$('#add-btn').click(function(e) {
+  var make = $("#inputMake2").val();
+  var model = $("#inputModel2").val();
+  var customer = parseInt($("#inputCustomer2").val());
+  
+  $.ajax ({  
+    headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+    url:'/cars',
+    type: 'POST',
+    datatype: 'JSON',
+    contentType: "application/json",
+    data: JSON.stringify({
+      "make":make, "model":model, "owner_id":customer}), 
+    success: function(cars) {    
+      $(".container").html($("#view-cars").html())
+      showCars();
+    }
+  })
+});
+
+//save edit
+$('#save-btn').click(function(e) {
+ var id = parseInt($("#inputId").val());   
+ var make = $("#inputMake").val();
+ var model = $("#inputModel").val();
+ var customer = parseInt($("#inputCustomer").val());
+ 
+ $.ajax ({
+  headers : {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token},
+  url:'/cars/' + id,
+  type: 'PUT',
+  datatype: 'JSON',
+  contentType: "application/json",
+  data: JSON.stringify({
+    "make":make, "model": model, "owner_id":customer}),
+  success: function(cars) {    
+    alert("bil ändrad");
+    $(".container").html($("#view-cars").html())
+    showCars();
+  }
+}) 
+
+});
+
+
+function submitmsg() {
+  alert($("#inputName").val() + "\n"+ $("#inputMail").val()+ "\n" + $("#inputMessage").val());
+}
+
+function signUp() {
+  var name = $("#inputName2").val();
+  var mail = $("#inputMail2").val();
+  var password = $("#inputPassword").val();
+ 
+  $.ajax ({  
+    url:'/sign-up',
+    type: 'POST',
+    datatype: 'JSON',
+    contentType: "application/json",
+    data: JSON.stringify({
+      "name":name, "mail":mail, "password":password}), 
+    success: function(i) { 
+         alert("Din användare är skapad!");
+         $(".container").html($("#view-home").html());
+    }
+  });
+ 
+}
+
+function logIn() {
+  var mail = $("#inputMail3").val();
+  var password = $("#inputPassword2").val();
+ 
+  $.ajax ({  
+    url:'/login',
+    type: 'POST',
+  datatype: 'JSON',
+    contentType: "application/json",
+    data: JSON.stringify({
+      "name":name, "mail":mail, "password":password}), 
+    success: function(i) { 
+        
+     sessionStorage.setItem('auth', JSON.stringify(i));     
+     $(".container").html($("#view-home").html());
+     window.location.reload(); 
+       
+    },
+    error: function(){
+      alert("Wrong username or password!");
+    }
+    // statuscode: {
+    //   401: function() {
+    //     alert("Wrong username or password!");
+    //     $(".container").html($("#view-home").html());
+
+    //   }
+    //}
+  });
+}
---
+++ labfiles/main.py
@@ -0,0 +1,248 @@
+#comment for forced change
+from flask import Flask, abort
+from flask import jsonify
+from flask import request
+from sqlalchemy import null
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
+app = Flask(__name__, static_folder='../client', static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = "svargissadstrang"
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+
+class Car(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  make = db.Column(db.String, nullable=False)
+  model = db.Column(db.String, nullable=False)
+  owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+
+  def __repr__(self):
+    return '<Car {}: {} {} {}>'.format(self.id, self.make, self.model)
+
+  def serialize(self):
+    return dict(id=self.id, make=self.make, model=self.model)
+
+  def serialize2(self):
+    return dict(id=self.id, make=self.make, model=self.model, owner=self.user.serialize())
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  mail = db.Column(db.String, nullable=False)
+  car_id = db.relationship('Car', backref='user', lazy=True)
+  is_admin = db.Column(db.Boolean, default=False, nullable=False)
+  password_hash = db.Column(db.String, nullable=False)
+
+  def __repr__(self):
+    return '<User {}: {} {}>'.format(self.id, self.name, self.mail)
+
+  def serialize(self):
+    return dict(id=self.id, name=self.name, isadmin = self.is_admin, mail=self.mail)
+
+  def set_password(self, password):
+    self.password_hash= bcrypt.generate_password_hash(password).decode('utf8')
+
+   
+
+@app.route("/login", methods= ['POST'])
+def login():
+  if request.method == 'POST':
+    indata = request.get_json()
+    users = User.query.all()
+    for x in users:
+      if x.mail == indata["mail"]:
+       
+        if bcrypt.check_password_hash(x.password_hash, indata['password']):         
+          access_token = create_access_token(identity = x.serialize())
+          dict = {"token" : access_token,
+                  "user" : x.serialize()
+                  }
+
+          return jsonify(dict), 200
+        else:
+          return "wrong password", 401
+    
+    return "no such user", 401
+  
+
+
+
+@app.route("/")
+def client():
+  return app.send_static_file("client.html")
+
+@app.route('/sign-up', methods= ['GET', 'POST'])
+def signup():
+
+  if request.method == 'POST':
+    new_user = request.get_json()
+    x = User(name = new_user["name"], mail = new_user["mail"])
+    x.set_password(new_user["password"])
+    db.session.add(x)
+    db.session.commit()
+    user_id = x.id
+    i = User.serialize(User.query.get_or_404(user_id))
+    return i
+
+@app.route('/cars/<int:car_id>', methods= ['PUT', 'GET', 'DELETE'])
+@jwt_required()
+def cars(car_id):
+  if request.method == 'GET':
+    temp = Car.query.filter_by(id = car_id).first_or_404()
+    if temp.user is not None:
+      return jsonify(temp.serialize2())
+    else:
+      return jsonify(temp.serialize())
+  elif request.method == 'PUT':
+     car = request.get_json()
+     car["id"]= car_id
+     Car.query.filter_by(id = car_id).update(car)     
+     temp = Car.query.filter_by(id = car_id).first_or_404()
+    
+     
+     db.session.commit()
+     
+     return jsonify(temp.serialize())
+  elif request.method == 'DELETE':
+    new_car = Car.query.get_or_404(car_id)
+    db.session.delete(new_car)
+    db.session.commit()
+    return "OK", 200
+
+@app.route('/carsb/<int:car_id>/booking', methods= ['POST'])
+@jwt_required()
+def carsb(car_id):
+  
+  if request.method == 'POST':
+    temp = Car.query.filter_by(id = car_id).first_or_404()
+    x = int(get_jwt_identity().get('id'))
+    if temp.user is None:
+      setattr(temp, "owner_id", x)
+      db.session.commit()       
+      return "success : true"
+    else:
+      return "success : false"
+
+@app.route('/carsb/<int:car_id>/unbook', methods= ['POST'])
+@jwt_required()
+def carsub(car_id):
+  
+  if request.method == 'POST':
+    temp = Car.query.filter_by(id = car_id).first_or_404()
+    x = int(get_jwt_identity().get('id'))
+    if temp.owner_id == x:
+      setattr(temp, "owner_id", None)
+      db.session.commit()       
+      return "Avbokad"
+    else:
+      return "Inte din bokning"
+
+
+
+
+# @app.route('/user/<int:user_id>/cars', methods=['GET'])
+# @jwt_required()
+# def user_cars(user_id):
+#   if request.method == 'GET':
+#     temp = User.query.filter_by(id = user_id).first_or_404()
+#     car_list = []
+#     if temp.car_id is not None:
+#       for x in temp.car_id:
+#         car_list.append(x.serialize())
+#     return jsonify(car_list)
+
+
+
+
+@app.route('/user/<int:user_id>', methods= ['PUT', 'GET', 'DELETE'])
+@jwt_required()
+def users(user_id):
+  if request.method == 'GET':
+    temp = User.query.filter_by(id = user_id).first_or_404()
+    return jsonify(temp.serialize())
+  elif request.method == 'PUT':
+     
+     user = request.get_json()
+     user["id"]= user_id
+     User.query.filter_by(id = user_id).update(user)     
+     temp = User.query.filter_by(id = user_id).first_or_404()
+     
+     db.session.commit()
+     
+     return jsonify(temp.serialize())
+  elif request.method == 'DELETE':
+    temp_user = User.query.get_or_404(user_id)
+   
+
+    db.session.delete(temp_user)
+    db.session.commit()
+    return "200 OK"
+
+
+
+
+
+  
+
+@app.route('/cars', methods=['GET','POST'])
+@jwt_required()
+def car():
+  
+  if request.method == 'GET':
+    car = Car.query.all()
+    car_list = []
+    
+    for x in car:
+      
+      if x.owner_id is not None:
+        car_list.append(x.serialize2())
+      elif x.owner_id is None:
+        car_list.append(x.serialize())    
+    return jsonify(car_list)
+
+  elif request.method == 'POST': 
+    new_car = request.get_json()
+    x = Car(make = new_car["make"], model = new_car["model"], owner_id = new_car["owner_id"])
+    db.session.add(x)
+    db.session.commit()
+    car_id = x.id
+    i = Car.serialize(Car.query.get_or_404(car_id))
+    return i
+  
+
+  return "401"
+
+
+@app.route('/users', methods=['GET','POST'])
+@jwt_required()
+def user():
+  if request.method == 'GET':
+    user = User.query.all()
+    user_list = []
+    for x in user:
+      user_list.append(x.serialize())
+
+    return jsonify(user_list)
+  elif request.method == 'POST': 
+    new_user = request.get_json()
+    x = User(name = new_user["name"], mail = new_user["mail"])
+    db.session.add(x)
+    db.session.commit()
+    user_id = x.id
+    i = User.serialize(User.query.get_or_404(user_id))
+    return i 
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
