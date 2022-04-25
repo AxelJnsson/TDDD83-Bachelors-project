4fae0764ee35365f341809305c37af4a6d3a5df2
unknown <davca341@student.liu.se>
2022-04-25 17:09:24 +0200

    refaktorering

--- client/javascript/slide.js
+++ client/javascript/slide.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-//refaktorerad
-
-=======
 //REFAKTORERAD
 //functions for slideshow on homepage
->>>>>>> 3813c0a363fe212f77112d881a43ff686392fc9c
 let slideIndex = 1;
 showSlides();
 
@@ -22,20 +17,12 @@ function showSlides() {
   }
 
   slideIndex++;
-<<<<<<< HEAD
-  if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
-=======
   
   if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 4000); // Change image every 4 seconds
   }
->>>>>>> 3813c0a363fe212f77112d881a43ff686392fc9c
 
 // Next/previous controls
 function plusSlides(n) {
@@ -46,14 +33,9 @@ function nextSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-<<<<<<< HEAD
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-=======
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
 
->>>>>>> 3813c0a363fe212f77112d881a43ff686392fc9c
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -61,13 +43,7 @@ function nextSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-<<<<<<< HEAD
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-=======
 
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
->>>>>>> 3813c0a363fe212f77112d881a43ff686392fc9c
